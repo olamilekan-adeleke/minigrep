6e68dff5e53ca2216fc4bab7368b1311c7a576a9
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-10-11 17:16:14 +0100

    feat: set up and added test .txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_name = &args[2];
+
+    println!("Searching for `{}`", query);
+    println!("In file `{}`", file_name);
 }
