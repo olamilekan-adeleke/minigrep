daf4543b9e9421311a6527f500d87a4bf7c62c68
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-10-12 22:38:29 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,56 @@
 use std::error::Error;
 use std::fs;
 
+pub enum ConfigOptions {
+    CaseSensitive,
+    CaseInSensitive,
+    CountOccurrence,
+    WithLineNumber,
+    None,
+}
+
 pub struct Config {
     query: String,
     file_name: String,
+    option: ConfigOptions,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Usage: minigrep [query] [filename]");
+            return Err("Usage: minigrep [options] [query] [filename]");
         }
 
-        let query = args[1].clone();
-        let file_name = args[2].clone();
+        let option_or_query = args[1].clone();
 
-        Ok(Config { query, file_name })
+        let query;
+        let file_name;
+        let config_option;
+
+        if option_or_query.starts_with("-") {
+            config_option = Config::prase_str_to_options(args[1].clone());
+            query = args[2].clone();
+            file_name = args[3].clone();
+        } else {
+            config_option = ConfigOptions::None;
+            query = args[1].clone();
+            file_name = args[2].clone();
+        }
+
+        Ok(Config {
+            query,
+            file_name,
+            option: config_option,
+        })
+    }
+
+    fn prase_str_to_options(option: String) -> ConfigOptions {
+        match option.as_str() {
+            "-i" => ConfigOptions::CaseSensitive,
+            "-c" => ConfigOptions::CountOccurrence,
+            "-n" => ConfigOptions::WithLineNumber,
+            _ => ConfigOptions::CaseInSensitive,
+        }
     }
 }
 
