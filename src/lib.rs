c7a44cf7cca5e659444878abe8a1decea4717af3
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-10-12 21:09:18 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Not enough arguments");
+            return Err("Usage: minigrep [query] [filename]");
         }
 
         let query = args[1].clone();
@@ -21,7 +21,38 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.file_name)?;
-    println!("With text:\n{:?}", file_content);
+
+    for line in search(&config.query, &file_content) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+                        Rust:
+                        safe, fast, productive.
+                        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
