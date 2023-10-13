fdd1cb728b3b1b52b01578ff662f19a069129aa0
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-10-13 21:01:30 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt::Write;
 use std::fs;
 
 mod config;
@@ -31,7 +32,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     };
 
     for line in lines {
-        println!("{}", line);
+        let mut colored_text = String::new();
+        let query = &config.query;
+
+        for word in line.split(" ") {
+            if word.contains(query) {
+                let start_index = word.find(query).unwrap();
+                let end_index = start_index + query.len();
+
+                colored_text.write_str(&word[..start_index]).unwrap();
+                colored_text.write_str("\x1b[31m").unwrap();
+                colored_text
+                    .write_str(&word[start_index..end_index])
+                    .unwrap();
+                colored_text.write_str("\x1b[0m").unwrap();
+                colored_text.write_str(&word[end_index..]).unwrap();
+            } else {
+                colored_text.write_str(word).unwrap();
+            }
+
+            colored_text.write_str(" ").unwrap();
+        }
+
+        colored_text.to_string();
+        println!("{}", colored_text);
     }
 
     Ok(())
